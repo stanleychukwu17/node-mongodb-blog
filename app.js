1bff7ddfee18bf73bea33c284552d829ded3e99d
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-18 09:26:24 +0100

    changes

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     let john = 'we done finish ohh'
     Object.prototype.convertMan = function (arg) {
-        console.log(arguments.callee)
+        console.log(this)
+        this.map(element => {
+            console.log(element)
+        });
     }
 
     // console.log(Object.prototype )
     let amaka = document.querySelectorAll('.amaka')
     amaka.convertMan()
+    // amaka.forEach(element => {
+    //     console.log(element)
+    // });
 });
