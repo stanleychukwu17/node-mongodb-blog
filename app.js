aab92f158e98307d4876399000c047d3816371e2
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-18 15:15:20 +0100

    changes

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(8080);
 
+// for home page
 app.get('/', function(req, res) {
     // app.render('index', {'title':'john'});
     res.render('index', {'title':'john'});
+});
+
+// for about page
+app.get('/about-us', function(req, res) {
+    res.send('<p>About us</p>');
+});
+
+// for new blog page
+app.get('/new-blog', function(req, res) {
+    res.send('<p>new blog</p>');
 });
