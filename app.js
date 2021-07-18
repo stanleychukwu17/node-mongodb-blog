d184bfc465d32c1f4db07a305b6656fcc698ae5a
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-18 09:31:22 +0100

    changes

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     let john = 'we done finish ohh'
-    Object.prototype.convertMan = function (arg) {
+    Object.prototype.convertMan = function (action) {
         console.log(this)
-        this.map(element => {
-            console.log(element)
+        this.forEach(element => {
+            if (action == 'hide_div') { element.style.display = 'none'; }
+            else if (action == 'show_div') { element.style.display = 'block'; }
         });
     }
 
     // console.log(Object.prototype )
     let amaka = document.querySelectorAll('.amaka')
-    amaka.convertMan()
+    amaka.convertMan('hide_div')
+
+    document.querySelector('.show_me').addEventListener('click', () => {
+       amaka.convertMan('show_div');
+    })
     // amaka.forEach(element => {
     //     console.log(element)
     // });
