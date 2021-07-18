d793dfd7118a18bc31c8fb501b3785622d62a73e
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-18 13:26:48 +0100

    changes

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(8080);
 
 app.get('/', function(req, res) {
-    console.log('for home page again');
     // app.render('index', {'title':'john'});
     res.render('index', {'title':'john'});
 });
