96017d5f3bb756df20e270dc568b64427cfa5803
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-18 13:01:13 +0100

    changes

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    let john = 'we done finish ohh'
-    Object.prototype.convertMan = function (action) {
-        console.log(this)
-        this.forEach(element => {
-            if (action == 'hide_div') { element.style.display = 'none'; }
-            else if (action == 'show_div') { element.style.display = 'block'; }
-        });
-    }
+const express = require('express')
+const app = express()
 
-    // console.log(Object.prototype )
-    let amaka = document.querySelectorAll('.amaka')
-    amaka.convertMan('hide_div')
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
-    document.querySelector('.show_me').addEventListener('click', () => {
-       amaka.convertMan('show_div');
-    })
-    // amaka.forEach(element => {
-    //     console.log(element)
-    // });
+app.listen(8080);
+
+app.get('/', function(req, res) {
+    console.log('for home page again');
+    // app.render('index', {'title':'john'});
+    res.render('index', {'title':'john'});
 });
