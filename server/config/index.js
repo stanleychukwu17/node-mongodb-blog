ab34092b7081fd1cc0a600f975f5a05bcf25d725
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-21 15:41:07 +0100

    changes

---
+++ server/config/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+
+module.exports = {
+  development: {
+    sitename: 'Development phase',
+    database: {dsn: process.env.DEVELOPMENT_DB_DSN},
+  },
+  production: {
+    sitename: 'Production phase',
+    database: {dsn: process.env.PRODUCTION_DB_DSN},
+  }
+};
