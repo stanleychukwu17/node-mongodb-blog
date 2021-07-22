52ff439fe880d2fc0a482974b5078dd01ae94b7a
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-22 08:22:24 +0100

    changes

--- server/lib/connect.js
+++ server/lib/connect.js
@@ -5,29 +5,7 @@
  const app = require('../server/app')(config);
  const db = require('../server/lib/db');
  
- const log = config.log();
- 
- const numCPUs = os.cpus().length;
- // Helper functions
- 
- /**
-  * Normalize a port into a number, string, or false.
-  */
- function normalizePort(val) {
-   const port = parseInt(val, 10);
- 
-   if (Number.isNaN(port)) {
-     // named pipe
-     return val;
-   }
-   if (port >= 0) {
-     // port number
-     return port;
-   }
-   return false;
- }
- 
- 
+
  /**
   * Get port from environment and store in Express.
   */
