3c343e168efe8c6eaed28b35bcfbaaaf5f1fbb8f
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-22 08:26:56 +0100

    changes

--- server/lib/connect.js
+++ server/lib/connect.js
@@ -1,8 +1,8 @@
  const http = require('http');
  const cluster = require('cluster');
  const os = require('os');
- const config = require('../server/config')[process.env.NODE_ENV || 'development'];
- const app = require('../server/app')(config);
+ const config = require('../config/index')[process.env.NODE_ENV || 'development'];
+//  const app = require('../server/app')(config);
  const db = require('../server/lib/db');
 
 
