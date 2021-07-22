def36b811e8c1d43a0953ce940c33028a353b763
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-22 08:27:12 +0100

    changes

--- server/lib/connect.js
+++ server/lib/connect.js
@@ -3,7 +3,7 @@
  const os = require('os');
  const config = require('../config/index')[process.env.NODE_ENV || 'development'];
 //  const app = require('../server/app')(config);
- const db = require('../server/lib/db');
+ const db = require('./db');
 
 
  console.log('called connect')
