8e533e8676805fc9e41bf8780628b5e1afb3b455
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-24 08:50:39 +0100

    changes

--- server/lib/auth.js
+++ server/lib/auth.js
@@ -2,5 +2,9 @@ const passport = require('passport')
 
 module.exports = {
     initialize: passport.initialize(),
-    
+    session: passport.session(),
+    setUser: (req, res, next) => {
+        res.locals.user = req.user;
+        return next();
+    }
 }
